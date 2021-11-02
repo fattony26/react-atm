3f2c33b0a8744ba14ac6e118c18e135141ce7f6b
fattony26 <marco.moran26@hotmail.com>
2021-11-01 22:22:22 -0400

    react atm steps 1-6 finished

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
-
 class Account extends Component {
+  state = {
+    balance: 0
+  }
+
+  depositClick = () => {
+    this.setState({
+      balance: parseInt(this.inputRef.current.value) + this.state.balance
+    })
+    this.inputRef.current.value ='';
+  }
+
+  withdrawClick = () => {
+    this.setState({
+      balance: parseInt(this.inputRef.current.value) - this.state.balance
+    })
+    this.inputRef.current.value ='';
+  }
+
+  inputRef = React.createRef();
+  
   render() {
+    console.log(this.inputRef)
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={this.inputRef} />
+        <input onClick={this.depositClick} type="button" value="Deposit" />
+        <input onClick={this.withdrawClick} type="button" value="Withdraw" />
       </div>
     )
   }
